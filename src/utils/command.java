e05ce13c079404dcfdb53386fdd818266ad21ad0
Kaleb Dufera <kalebdufera@Kalebs-MacBook-Pro-2.local>
2018-12-28 04:29:02 -0500

    added utils command class constants

---
+++ src/utils/command.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class command { // constant commands 
+	public static final String SET = "SET";
+	public static final String GET = "GET";
+	public static final String DELETE = "DELETE";
+	public static final String COUNT = "COUNT";
+	public static final String BEGIN = "BEGIN";
+	public static final String ROLLBACK = "ROLLBACK";
+	public static final String COMMIT = "COMMIT";
+	public static final String END = "END";
+}
